1aa6998312ff73b1432fcd900237207b19a3b821
SAGAR T <ssagu6661@gmail.com>
2024-08-17 12:12:00 -0400

    Add files via upload

---
+++ c13.py
@@ -0,0 +1,34 @@
+# # LIST PROGRAM
+# MY_LIST =[1,2,3,4,5]            
+# print(MY_LIST)          
+# MY_LIST[0]=10                 #GIVEN ORIGINAL NUMBER ,TO CHANGE THE GIVEN NUMBER  
+# print(MY_LIST)       # WE CAN CHANGE THE LIST
+
+
+
+print("simple list")
+print("we can't change the list value or elements in 'tupples()' ")
+print("\n")
+print("NUMBER LIST")
+sagar_list =("1233","2345","3456","4569")
+print(sagar_list)
+print("=========================")
+print("\n")
+print("After Adding Elements")
+sagar_list.insert(1, "1234")
+print(sagar_list)
+print("\n")
+print("=========================")
+print("After changing 1st list number ")
+sagar_list[0]=2023
+
+print(sagar_list)
+
+
+#sets
+# print("'SETS()'")
+# set1={1,2,3,4,5}
+# print(set1[0])
+# print("\n")
+
+
